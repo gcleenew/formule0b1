71c5d7c0a7605ce38484231b563c498fa2016ddb
vcostet <valentin.costet@isep.fr>
2013-12-05 11:21:20 +0100

    Add Map and Vehicle; Update Game

---
+++ src/Map.java
@@ -0,0 +1,6 @@
+public class Map {
+
+    Map(java.io.File file) {
+
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle extends Object {
+
+    private Vector2D speed;
+    private Vector2D acceleration;
+    private Map map;
+
+    public double move(double dt) {
+
+        return 0;
+    }
+}
